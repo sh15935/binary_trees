07223b007da83570c8f8eac6d9885104be0b18aa
sh15935 <safazsahih@gmail.com>
2023-11-03 21:30:06 +0000

    tasks

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -11,9 +11,9 @@
 int is_bst(const binary_tree_t *tree, int min, int max)
 {
 	if (!tree)
-		return 1;
+		return (1);
 	if (tree->n < min || tree->n > max)
-		return 0;
+		return (0);
 	return (is_bst(tree->left, min, tree->n - 1) &&
 		is_bst(tree->right, tree->n + 1, max));
 }
